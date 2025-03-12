8a7ff249a0df040afae3d5c0517b9f6bdf3a8c93
Juan Pedro Martin <juamp_m@hotmail.com>
2025-03-12 15:23:58 +0100

    feat: set up starknet provider with minimal config
    - Add `@starknet-react/chains` and `@starknet-react/core` as core deps
    - Add `get-starknet-core` and `starknet` as peer dependencies
    
    Note: For the project to work properly we have to install the correct
    version of the peer dependencies.

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./styles/globals.css";
-import { ThemeProvider } from "@/components/providers";
+import { StarknetProvider } from "@/components/providers/starknet-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          <StarknetProvider>{children}</StarknetProvider>
         </ThemeProvider>
       </body>
     </html>
